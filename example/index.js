cb50d5f2d4e59706bb7afc8437d20188c0a1b32d
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2022-08-27 12:47:16 +0200

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const urlShorter = require('./urlShorter.js');
+const urlShortener = require('express-shortener');
 
-urlShorter.configURL({
+urlShortener.configURL({
   file: "./urls.json",
   parameter: "id",
   logClicks: true
@@ -19,29 +19,29 @@ app.use((req, res, next) => {
 });
 
 app.all("/url/:id", (req, res) => {
-  urlShorter.openURL(req, res).then((result) => {});
+  urlShortener.openURL(req, res).then((result) => {});
 });
 
 app.all("/api/v1/url/create", (req, res) => {
-  urlShorter.createURL(req.body).then((result) => {
+  urlShortener.createURL(req.body).then((result) => {
     res.json(result);
   });
 });
 
 app.all("/api/v1/url/edit", (req, res) => {
-  urlShorter.editURL(req.body).then((result) => {
+  urlShortener.editURL(req.body).then((result) => {
     res.json(result);
   });
 });
 
 app.all("/api/v1/url/delete", (req, res) => {
-  urlShorter.deleteURL(req.body).then((result) => {
+  urlShortener.deleteURL(req.body).then((result) => {
     res.json(result);
   });
 });
 
 app.all("/api/v1/url/get", (req, res) => {
-  urlShorter.getURL(req.body).then((result) => {
+  urlShortener.getURL(req.body).then((result) => {
     res.json(result);
   });
 });
